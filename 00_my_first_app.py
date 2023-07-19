5ffce85b150e55cba32402af403962a520e84807
pachonas <133802948+pachonas@users.noreply.github.com>
2023-07-19 20:03:51 +0900

    Update 00_my_first_app.py

--- 00_my_first_app.py
+++ 00_my_first_app.py
@@ -1,79 +1,116 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
-)
 from langchain.callbacks import get_openai_callback
 
-# Set page configuration as the first command
-st.set_page_config(
-    page_title="My Great ChatGPT",
-    page_icon="🤗"
-)
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Qdrant
 
-def init_messages():
-    clear_button = st.sidebar.button("Clear Conversation", key="clear")
-    if clear_button or "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
-        ]
-        st.session_state.costs = []
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
 
-def select_model():
-    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
-    if model == "GPT-3.5":
-        model_name = "gpt-3.5-turbo"
+QDRANT_PATH = "./local_qdrant"
+COLLECTION_NAME = "my_collection"
+
+
+def init_page():
+    st.set_page_config(
+        page_title="Ask My PDF(s)",
+        page_icon="🤗"
+    )
+    st.sidebar.title("Nav")
+    st.session_state.costs = []
+
+
+def get_pdf_text():
+    uploaded_file = st.file_uploader(
+        label='Upload your PDF here😇',
+        type='pdf'
+    )
+    if uploaded_file:
+        pdf_reader = PdfReader(uploaded_file)
+        text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name=st.session_state.emb_model_name,
+            # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
+            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
+            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
+            chunk_size=250,
+            chunk_overlap=0,
+        )
+        return text_splitter.split_text(text)
     else:
-        model_name = "gpt-4"
-
-    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
-
-    return ChatOpenAI(temperature=temperature, model_name=model_name)
+        return None
 
 
-def get_answer(llm, messages):
-    with get_openai_callback() as cb:
-        answer = llm(messages)
-    return answer.content, cb.total_cost
+def load_qdrant():
+    client = QdrantClient(path=QDRANT_PATH)
+
+    # すべてのコレクション名を取得
+    collections = client.get_collections().collections
+    collection_names = [collection.name for collection in collections]
+
+    # コレクションが存在しなければ作成
+    if COLLECTION_NAME not in collection_names:
+        # コレクションが存在しない場合、新しく作成します
+        client.create_collection(
+            collection_name=COLLECTION_NAME,
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+        )
+        print('collection created')
+
+    return Qdrant(
+        client=client,
+        collection_name=COLLECTION_NAME, 
+        embeddings=OpenAIEmbeddings()
+    )
+
+
+def build_vector_store(pdf_text):
+    qdrant = load_qdrant()
+    qdrant.add_texts(pdf_text)
+
+    # 以下のようにもできる。この場合は毎回ベクトルDBが初期化される
+    # LangChain の Document Loader を利用した場合は `from_documents` にする
+    # Qdrant.from_texts(
+    #     pdf_text,
+    #     OpenAIEmbeddings(),
+    #     path="./local_qdrant",
+    #     collection_name=COLLECTION_NAME,
+    # )
+
+
+def page_pdf_upload_and_build_vector_db():
+    st.title("PDF Upload")
+    container = st.container()
+    with container:
+        pdf_text = get_pdf_text()
+        if pdf_text:
+            with st.spinner("Loading PDF ..."):
+                build_vector_store(pdf_text)
+
+
+def page_ask_my_pdf():
+    st.title("Ask My PDF(s)")
+    st.write('Under Construction')
+
+    # 後で実装する
+
 
 def main():
+    init_page()
 
-    st.header("My Great ChatGPT 🤗")
-    st.sidebar.title("Options")
+    selection = st.sidebar.radio("Go to", ["PDF Upload", "Ask My PDF(s)"])
+    if selection == "PDF Upload":
+        page_pdf_upload_and_build_vector_db()
+    elif selection == "Ask My PDF(s)":
+        page_ask_my_pdf()
 
-    llm = select_model()
-    init_messages()
-
-    user_input = st.chat_input("聞きたいことを入力してね！")
-    if user_input:
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("ChatGPT is typing ..."):
-            answer, cost = get_answer(llm, st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=answer))
-        st.session_state.costs.append(cost)
-
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message('assistant'):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message('user'):
-                st.markdown(message.content)
-        else:  # isinstance(message, SystemMessage):
-            st.write(f"System message: {message.content}")
-
-EXCHANGE_RATE = 110
-
-costs = st.session_state.get('costs', [])
-st.sidebar.markdown("## Costs")
-total_cost_in_jpy = sum(costs) * EXCHANGE_RATE
-st.sidebar.markdown(f"**Total cost: ¥{total_cost_in_jpy:.5f}**")
-for cost in costs:
-    cost_in_jpy = cost * EXCHANGE_RATE
-    st.sidebar.markdown(f"- ¥{cost_in_jpy:.5f}")
+    costs = st.session_state.get('costs', [])
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
+    for cost in costs:
+        st.sidebar.markdown(f"- ${cost:.5f}")
 
 if __name__ == '__main__':
     main()
